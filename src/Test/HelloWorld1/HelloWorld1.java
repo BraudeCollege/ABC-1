ae8c9ced569c3550ca9793a3dd918ea06a73e127
Parth Vikani <allinhtml@gmail.com>
2017-04-21 21:49:37 +0530

    Added HelloWorld1 program

---
+++ src/Test/HelloWorld1/HelloWorld1.java
@@ -0,0 +1,11 @@
+/******************************************************************************
+ * Compilation:  javac HelloWorld1.java
+ * Execution:    java HelloWorld1
+ * Dependencies: -
+ * Author:		  Parth Vikani
+ ******************************************************************************/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world1!");
+    }
+}
